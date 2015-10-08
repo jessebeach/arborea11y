947372174520ff3484a31587967b8ef6510b2100
Jesse Renee Beach <splendidnoise@gmail.com>
2015-10-08 12:56:01 -0700

    Append AXTree at the bottom of the body.

--- content.js
+++ content.js
@@ -5,8 +5,11 @@ const COMPLETE = 'complete';
 function onExtensionMessage(response) {
   if (response.axtree) {
     let axTreeFrag = DocFragUtils.deserialize(response.axtree);
-    document.body.innerHTML = '';
-    document.body.appendChild(axTreeFrag);
+    let axContainer = document.createElement('div');
+    axContainer.setAttribute('id', 'arborea11y-container');
+    axContainer.setAttribute('style', 'outline: 1px solid red; outline-offset: -1px; padding: 10px; margin: 10px 0;');
+    axContainer.appendChild(axTreeFrag);
+    document.body.appendChild(axContainer);
   }
 }
 
