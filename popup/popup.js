32a639943e6df90f9bd66aa8e73f907226b82093
Jesse Renee Beach <splendidnoise@gmail.com>
2015-10-08 13:20:01 -0700

    Use sane Tab selection so we're not tossing messages to all windows.

--- popup/popup.js
+++ popup/popup.js
@@ -4,13 +4,16 @@ document.addEventListener('DOMContentLoaded', function () {
   var button = document.getElementById('loadAXTreeButton');
   button.addEventListener('click', function () {
     let button = event.target;
-    Tabs.getCurrent().then(function(tabInfo) {
-      chrome.runtime.sendMessage(
-        {
-          action: 'replace',
-          tabId: tabInfo.id
-        }
-      );
-    });
+    Tabs.getCurrent().then(
+      // Success.
+      function(tabInfo) {
+        chrome.runtime.sendMessage(
+          {
+            action: 'replace',
+            tabId: tabInfo.id
+          }
+        );
+      }
+    );
   }, false);
 });
